8f813d923c0ff3cd79a33a8671fcfb2b40a88ed8
camdensaks <54951342+camdensaks@users.noreply.github.com>
2019-09-20 09:25:19 -0700

    Add files via upload

--- Store.java
+++ Store.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
+import java.util.Random;
+import java.text.DecimalFormat;
+public class Store {
 
-Scanner input = new Scanner(System.In);
-
-public class Receipt {
     public static void main(String [] args){
-      Scanner input = new Scanner(System.In);
-      System.out.printIn("Number of books bought");
-      int BooksBought = input.nextInt();
-      System.out.printIn("Number of movies bought")
-      int MoviesBought = input.nextInt();
-      System.out.printIn("Pounds of peanuts bought");
-      int PeanutsBought = input.nextInt();
-
-      int month = Math.floor(1 + Math.random()* 12);
-      int days = Math.floor(1 + Math.random()* 31);
-      int years = Math.floor(1 + Math.random()* 99)
-
+      Scanner input = new Scanner(System.in);
+      String store = "Steve's Mart";
+      String cashierName = "Steve";
+      System.out.println(store);
+      System.out.println(cashierName);
+      Random rand = new Random();
+      System.out.println("Month: " + (rand.nextInt(12) + 1));
+      System.out.println("Day: " + (rand.nextInt(28) + 1));
+      System.out.println("Time: " + (rand.nextInt(24)));
+      System.out.println("Year: 2019 ");
       double books = 9.0;
       double movies = 13.97;
       double peanuts = 1.72;
@@ -25,10 +23,23 @@ public class Receipt {
       double shippeanuts = 0.30;
       double tax = 0.072;
 
-      double bookstotal = (books * BooksBought * tax) + (shipbook * BooksBought)
-      double moviestotal = (movies * MoviesBought * tax) + (shipmovies * MoviesBought)
-      double peanutstotal = (peanuts * PeanutsBought * tax) + (shippeanuts * PeanutsBought)
+      System.out.println("Number of books bought: " );
+      int BooksBought = input.nextInt();
+      System.out.println("Number of movies bought: " );
+      int MoviesBought = input.nextInt();
+      System.out.println("Pounds of peanuts bought: " );
+      int PeanutsBought = input.nextInt();
 
+      System.out.println("Book Shipping Cost: " + shipbook);
+
+      double bookstotal = (books * BooksBought) + (shipbook * BooksBought);
+      double moviestotal = (movies * MoviesBought) + (shipmovies * MoviesBought);
+      double peanutstotal = (peanuts * PeanutsBought) + (shippeanuts * PeanutsBought);
+      double grandtotal = (bookstotal + moviestotal + peanutstotal);
+      double salestotal = (grandtotal * tax);
+      double finaltotal = (grandtotal + salestotal);
+      System.out.println("Grand Total: " + grandtotal);
+      System.out.println("Sales Tax: " + tax);
+      System.out.println("Final Total: " + finaltotal);
     }
-
 }
